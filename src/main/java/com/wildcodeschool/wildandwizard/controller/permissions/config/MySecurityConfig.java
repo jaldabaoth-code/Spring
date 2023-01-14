09c36c0201be9fb17e86e4efde11a7c151c4edef
jaldabaoth-code <grialazurabi@gmail.com>
2023-01-14 01:16:18 +0100

    save

--- src/main/java/com/wildcodeschool/wildandwizard/controller/permissions/config/MySecurityConfig.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/permissions/config/MySecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-/* Spring Quest : Permissions (Config) */
+/* Quest : Permissions */
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
